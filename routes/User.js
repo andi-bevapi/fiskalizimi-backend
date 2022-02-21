758a98a39ec31392964ebf9370b63b7dce928d6f
marselina_d <dollapajmarselinda@gmail.com>
2022-02-21 11:57:35 +0100

    userss

--- routes/User.js
+++ routes/User.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/UserController");
+const Joivalidation = require("../validation/user");
 
 router.get("/", userController.getAllUsers);
-router.post("/create", userController.createUser);
-router.put("/update/:id", userController.updateUser);
+router.post("/create", Joivalidation, userController.createUser);
+router.put("/update/:id", Joivalidation, userController.updateUser);
 router.put("/delete/:id", userController.deleteUser);
 
 module.exports = router;
-
