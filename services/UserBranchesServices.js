a1950416c4d5c59a0241b9058a9bfa175ff0d520
marselina_d <dollapajmarselinda@gmail.com>
2022-02-28 15:18:07 +0100

    userbranches update

--- services/UserBranchesServices.js
+++ services/UserBranchesServices.js
@@ -5,8 +5,6 @@ const getUserBranches = async () => {
   // console.log(user);
   // console.log(branches);
   const allBranches = await UserBranches.findAll();
-  console.log("ALL BRANCHES==", allBranches);
-  console.log("before create");
   return allBranches;
 };
 
@@ -25,8 +23,58 @@ const createUserBranches = async (user, branches) => {
 };
 
 const updateUserBranches = async (id, user) => {
-  const allBranches = await UserBranches.findAll({where: {userId: id}}, {raw: true});
-  console.log(allBranches);
+  let allBranches = await UserBranches.findAll(
+    { where: { userId: id } },
+    { raw: true }
+  );
+  let newBranchesId = user.branches;
+  for (var branch of user.branches) {
+    const userBranch = await UserBranches.findAll(
+      { where: { userId: id, branchId: branch } },
+      { raw: true }
+    );
+    if (userBranch.length > 0) {
+      allBranches = allBranches.filter(
+        (el) => el.dataValues.id != userBranch[0].id
+      );
+      newBranchesId = newBranchesId.filter((el) => el != branch);
+    }
+  }
+  console.log("allBranches", allBranches);
+  console.log("NEW BRANCH ID", newBranchesId);
+  if (allBranches.length == newBranchesId.length) {
+    let index = 0;
+    for (var branch of allBranches) {
+      const userToUpdate = await UserBranches.update(
+        { branchId: newBranchesId[index] },
+        {
+          where: {
+            id: branch.dataValues.id,
+          },
+        }
+      );
+      index++;
+    }
+  }
+  if (allBranches.length > newBranchesId.length) {
+    console.log("DELETE USER BRANCHES");
+    for (var branch of allBranches) {
+      console.log("BRANCH ID", branch.dataValues.id);
+      const userBranchToDelete = await UserBranches.destroy({
+        where: { id: branch.dataValues.id, userId: id },
+      });
+      console.log(userBranchToDelete);
+    }
+  }
+  if (allBranches.length < newBranchesId.length) {
+    console.log("NEW BRANCHES");
+    for (var newBranchId of newBranchesId) {
+      const newUserBranch = await UserBranches.create({
+        userId: id,
+        branchId: newBranchId,
+      });
+    }
+  }
 };
 
 const deleteUserBranches = async (id) => {
