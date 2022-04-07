77282bce484b3d38312ce066adce70105186613c
marselina_d <dollapajmarselinda@gmail.com>
2022-04-07 13:07:47 +0200

    change end date time

--- services/ReportsService.js
+++ services/ReportsService.js
@@ -2,7 +2,7 @@ const sequelize = require('../db');
 
 const getAllDashboardReports = async (clientId, start, end) => {
     const startDate = `${start} 00:00:00`;
-    const endDate = `${end} 00:00:00`;
+    const endDate = `${end} 23:59:59`;
 
     return await sequelize.query(
         'SELECT sum(totalAmount) as totalAmount, sum(totalVat) as totalVat, count(1) as totalInvoices from Invoices where clientId = :clientId AND createdAt >= :startDate AND createdAt <= :endDate GROUP by clientId',
@@ -15,7 +15,7 @@ const getAllDashboardReports = async (clientId, start, end) => {
 
 const getAllChartReports = async (clientId, start, end) => {
     const startDate = `${start} 00:00:00`;
-    const endDate = `${end} 00:00:00`;
+    const endDate = `${end} 23:59:59`;
 
     return await sequelize.query(
         'SELECT date(createdAt) as dateCreated, sum(totalAmount) as totalAmount, count(1) as totalInvoices from Invoices where clientId = :clientId AND createdAt >= :startDate AND createdAt <= :endDate GROUP by dateCreated',
@@ -28,7 +28,7 @@ const getAllChartReports = async (clientId, start, end) => {
 
 const getAllAnalyticsReports = async (clientId, start, end) => {
     const startDate = `${start} 00:00:00`;
-    const endDate = `${end} 00:00:00`;
+    const endDate = `${end} 23:59:59`;
 
     return await sequelize.query(
         'SELECT * from Invoices where clientId = :clientId AND createdAt >= :startDate AND createdAt <= :endDate',
