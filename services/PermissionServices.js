b132088c06e24bf4b873d099d80c4b04c3bc43aa
marselina_d <dollapajmarselinda@gmail.com>
2022-05-27 12:47:41 +0200

    remove clients permissions

--- services/PermissionServices.js
+++ services/PermissionServices.js
@@ -37,10 +37,6 @@ const createPermissions = async () => {
     { name: 'permission.arka.create' },
     { name: 'permission.arka.update' },
     { name: 'permission.arka.delete' },
-    { name: 'permission.client.view' },
-    { name: 'permission.client.create' },
-    { name: 'permission.client.update' },
-    { name: 'permission.client.delete' },
     { name: 'permission.reports.view' },
   ]
 
